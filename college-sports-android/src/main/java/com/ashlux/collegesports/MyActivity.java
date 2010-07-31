f54a1fbd67624c3f09b01ef780b3a889ec956ae4
Ash Lux <ashlux@gmail.com>
2010-07-30 22:47:37 -0500

    android app depends on rest api

--- college-sports-android/src/main/java/com/ashlux/collegesports/MyActivity.java
+++ college-sports-android/src/main/java/com/ashlux/collegesports/MyActivity.java
@@ -2,17 +2,30 @@ package com.ashlux.collegesports;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.ashlux.sportsscores.domain.Game;
+import com.ashlux.sportsscores.services.ScheduleDao;
+import org.restlet.resource.ClientResource;
 
 public class MyActivity
     extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.main );
+
+        // Example client
+        System.out.println(1);
+        ClientResource cr = new ClientResource(
+//                "http://localhost:8080/rest/schedules");
+                  "http://sportsscoresfeed.appspot.com/rest/schedules");
+        System.out.println(2);
+         ScheduleDao scheduleDao = cr.wrap( ScheduleDao.class );
+        System.out.println(3);
+        System.out.println(scheduleDao);
+        System.out.println(4);
+        Game[] games = scheduleDao.getSchedule();
+        System.out.println(games.length);
     }
 }
